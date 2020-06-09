218188fa1719029ba51bdc224df1c0a1eccecc2d
Victor Moreira <victormoreira176@hotmail.com>
2020-06-09 19:06:09 -0300

    making blockchain API call and printing it

---
+++ README.md
@@ -0,0 +1,1 @@
+# polybar-bitcoin-wallet
---
+++ bitcoin.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+// const read = require('readline');
+const wallet_pk = "3GU9DTPj9YVtQwz3KihPhByesDqP1Ywpuc";
+const address = `https://blockchain.info/balance?active=${wallet_pk}`;
+
+function BTCvalue(satoshi_value) {
+    const satoshi = 100000000;
+    return satoshi_value/satoshi;
+}
+
+async function getBalance(address) {
+    axios.get(address).then( res => {
+        const value = res.data[wallet_pk].final_balance;
+        console.log(BTCvalue(value));
+    });
+}
+
+getBalance(address);
