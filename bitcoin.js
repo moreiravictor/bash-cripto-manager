22ac9ff2cc2a1dfd59b3af690a16fd8bcfd2d58a
Victor Moreira <victormoreira176@hotmail.com>
2020-07-30 01:18:20 -0300

    adding the possibility to consult more than one wallet

--- bitcoin.js
+++ bitcoin.js
@@ -1,22 +1,32 @@
 const axios = require('axios');
-const wallet_pk = "11dcwyCsHw8AB45aHTd9cHUGZVVwR9h41";
+const wallet_keys = ["11dcwyCsHw8AB45aHTd9cHUGZVVwR9h41", "165wEp4BjiTWAuPcRbfjAwCFs6crUk9Huq"];
 const icon = "฿";
-const address = `https://blockchain.info/balance?active=${wallet_pk}`;
+const address = `https://blockchain.info/balance?active=${wallet_keys}`;
 
-function BTCvalue(satoshi_value) {
+function BTCvalue(satoshi_values) {
     const satoshi = 100000000;
-    return satoshi_value/satoshi;
+    let converted_values = [];
+    satoshi_values.forEach(value => converted_values.push(value/satoshi));
+    return converted_values.join(' | ');
+}
+
+function getFinalValues(wallets) {
+    let values = [];
+    for (let public_key in wallets) {
+        values.push(wallets[public_key].final_balance);
+    }
+    return values;
 }
 
 async function getBalance(address) {
     axios.get(address).then( res => {
-        const value = res.data[wallet_pk].final_balance;
-        console.log(`${icon} ${BTCvalue(value)}`);
+        let values = getFinalValues(res.data);
+        console.log(`${icon} ${BTCvalue(values)}`);
     }).catch(error => {
         if (error.code === "ENOTFOUND") {
             console.log("Reconnecting...");
         } else {
-            console.log("Inform error to maintainer:", error.code);
+            console.log("Inform error to maintainer:", error);
         }
     });
 }
