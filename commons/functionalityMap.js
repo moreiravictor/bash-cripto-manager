ba377295525d49dc450412ba060c03d38d09a263
Victor Moreira <victormoreira176@hotmail.com>
2021-02-02 19:52:12 -0300

    :sparkles: adding default option to functionalities

--- commons/functionalityMap.js
+++ commons/functionalityMap.js
@@ -4,7 +4,8 @@ const {getBalance} = require('../service/bitcoinWalletsManager');
 const functionalityMap = Object.freeze({
     '-p': priceAndPercentage,
     '-bw': getBalance,
-    '-t': userTotal
+    '-t': userTotal,
+    default: () => {return 'option not available'}
 })
 
 module.exports = {
--- manager.js
+++ manager.js
@@ -2,8 +2,7 @@ const {functionalityMap} = require('./commons/functionalityMap')
 
 const options = process.argv.slice(2);
 const functionality = options[0];
-const service = functionalityMap[functionality];
-
+const service = functionalityMap[functionality] || functionalityMap.default;
 (async function execute() {
     const info = await service();
     console.log(info);
